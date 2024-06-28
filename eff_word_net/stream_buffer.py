a3b154f5c23776e55e5800de9c90ca296e9ba88a
Your Name <you@example.com>
2024-06-28 19:46:28 +0800

    .

---
+++ eff_word_net/stream_buffer.py
@@ -0,0 +1,47 @@
+import threading
+from collections import deque
+import pyaudio
+
+import util
+
+
+class MicStream:
+    def __init__(self, stream, chuck_size=80, buffer_size=8192):
+        self.chunk_size = chuck_size
+        self.buffer_size = buffer_size
+        self.stream = stream
+        self.buffer = deque(maxlen=self.buffer_size)
+        self.is_stopped = False
+        self.lock = threading.Lock()
+        self.condition = threading.Condition(self.lock)
+
+    def start_stream(self):
+        self.is_stopped = False
+        threading.Thread(target=self._record_loop).start()
+
+    def _record_loop(self):
+        while not self.is_stopped:
+            chunk = self.stream.read(self.chunk_size, exception_on_overflow=False)
+            with self.condition:
+                self.buffer.append(chunk)
+                self.condition.notify_all()
+
+    def read(self, size):
+        if size % self.chunk_size != 0:
+            raise ValueError("Size must be a multiple of CHUNK_SIZE")
+        with self.condition:
+            while len(self.buffer) * self.chunk_size < size and not self.is_stopped:
+                self.condition.wait()
+
+            if self.is_stopped:
+                return None
+            chunks = []
+            while len(chunks) * self.chunk_size < size:
+                chunks.append(self.buffer.popleft())
+            util.print("return " + str(len(chunks)))
+            return b''.join(chunks)
+
+    def stop_stream(self):
+        self.is_stopped = True
+        with self.condition:
+            self.condition.notify_all()
--- eff_word_net/streams.py
+++ eff_word_net/streams.py
@@ -1,44 +1,42 @@
 import pyaudio
-from typing import Tuple , Callable
+from typing import Tuple, Callable
 import numpy as np
 from eff_word_net.engine import HotwordDetector
 from eff_word_net import RATE
+from eff_word_net.stream_buffer import MicStream
 
-NoParameterFunction = Callable[[],None]
-AudioFrameFunction = Callable[[],np.array]
+NoParameterFunction = Callable[[], None]
+AudioFrameFunction = Callable[[], np.array]
 
-RESPEAKER_INDEX = 6
-
-
-class CustomAudioStream :
+class CustomAudioStream:
     """
     CustomAudioStream implementation allows developers to use 
     any 16000Hz sampled audio streams with inference engine
 
     It tries to add sliding window to audio streams
     """
-    def __init__(
-        self,
-        open_stream:Callable[[],None],
-        close_stream:Callable[[],None],
-        get_next_frame:Callable[[],np.array],
-        window_length_secs = 1,
-        sliding_window_secs:float = 1/8
-        ):
 
+    def __init__(
+            self,
+            open_stream: Callable[[], None],
+            close_stream: Callable[[], None],
+            get_next_frame: Callable[[], np.array],
+            window_length_secs=1,
+            sliding_window_secs: float = 1 / 8
+    ):
         self._open_stream = open_stream
         self._close_stream = close_stream
         self._get_next_frame = get_next_frame
         self._window_size = int(window_length_secs * RATE)
         self._sliding_window_size = int(sliding_window_secs * RATE)
 
-        self._out_audio = np.zeros(self._window_size) #blank 1 sec audio
-        print("Initial S",self._out_audio.shape)
+        self._out_audio = np.zeros(self._window_size)  #blank 1 sec audio
+        print("Initial S", self._out_audio.shape)
 
     def start_stream(self):
         self._out_audio = np.zeros(self._window_size)
         self._open_stream()
-        for i in range(RATE//self._sliding_window_size -1):
+        for i in range(RATE // self._sliding_window_size - 1):
             self.getFrame()
 
     def close_stream(self):
@@ -57,49 +55,46 @@ class CustomAudioStream :
         assert new_frame.shape == (self._sliding_window_size,), \
             "audio frame size from src doesnt match sliding_window_secs"
 
-
         self._out_audio = np.append(
-                self._out_audio[self._sliding_window_size:],
-            new_frame 
+            self._out_audio[self._sliding_window_size:],
+            new_frame
         )
 
         # print(self._out_audio.shape)
 
         return self._out_audio
 
-class SimpleMicStream(CustomAudioStream) :
 
+class SimpleMicStream(CustomAudioStream):
     """
     Implements mic stream with sliding window, 
     implemented by inheriting CustomAudioStream
     """
-    def __init__(self,window_length_secs=1.5, sliding_window_secs:float=1/8):
-        p=pyaudio.PyAudio()
 
-        CHUNK = int(sliding_window_secs*RATE)
+    def __init__(self, window_length_secs=1.5, sliding_window_secs: float = 1 / 8):
+        p = pyaudio.PyAudio()
+
+        CHUNK = int(sliding_window_secs * RATE)
         print("Chunk size", CHUNK)
 
-
-        self.mic_stream = p.open(
+        self.mic_stream = MicStream(p.open(
             rate=16000,
             format=pyaudio.paInt16,
             channels=1,
             input=True,
-            # input_device_index=RESPEAKER_INDEX,
             frames_per_buffer=CHUNK
 
-        )
-
+        ))
         self.mic_stream.stop_stream()
 
         CustomAudioStream.__init__(
             self,
-            open_stream = self.mic_stream.start_stream,
-            close_stream = self.mic_stream.stop_stream,
-            get_next_frame = lambda : (
-                np.frombuffer(self.mic_stream.read(CHUNK,exception_on_overflow = False),dtype=np.int16)
-                ),
-                 window_length_secs=window_length_secs,
-                sliding_window_secs=sliding_window_secs
+            open_stream=self.mic_stream.start_stream,
+            close_stream=self.mic_stream.stop_stream,
+            get_next_frame=lambda: (
+                np.frombuffer(self.mic_stream.read(CHUNK), dtype=np.int16)
+            ),
+            window_length_secs=window_length_secs,
+            sliding_window_secs=sliding_window_secs
 
         )
