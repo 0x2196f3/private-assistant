ca32cf078196596f5615438b2bf676cd594b5aab
0x2196f3 <0x2196f3@gmail.com>
2024-07-14 05:06:22 +0800

    .

--- funasr.py
+++ funasr.py
@@ -1,72 +1,78 @@
 import json
-
-import websocket
-import threading
+import websockets
 
 import util
 
+import asyncio
+import threading
+import queue
+
+
+import asyncio
+import threading
+import queue
 
 class FunASR:
-    def __init__(self, url: str, on_receive):
+    def __init__(self, url, on_receive):
         self.url = url
         self.on_receive = on_receive
-        self.ws = None
-        self.stopped = True
-        self.lock = threading.Lock()
+        self.stop_event = threading.Event()
+        self.send_queue = queue.Queue()
         self.thread = None
+        self.ws = None
 
-    def connect(self):
-        if self.thread:
+    def start(self):
+        if self.thread is not None:
             self.stop()
-        self.thread = threading.Thread(target=self._connect)
-        self.thread.daemon = True
+        self.send_queue.queue.clear()
+        self.stop_event.clear()
+        self.thread = threading.Thread(target=self.run_loop)
         self.thread.start()
 
-    def _connect(self):
-        self.stopped = False
-        self.ws = websocket.WebSocketApp(self.url, on_open=self.on_open, on_message=self.on_message,
-                                         on_error=self.on_error, on_close=self.on_close)
-        try:
-            self.ws.run_forever(reconnect=5)
-        except Exception as e:
-            print(e)
-        finally:
-            self.stopped = True
-            with self.lock:
-                try:
-                    if self.ws:
-                        self.ws.close()
-                except:
-                    pass
-
-    def on_open(self, ws):
-        util.print("Opened connection")
-
-    def on_message(self, ws, message):
-        meg = json.loads(message)
-        text = meg["text"]
-        print(str(meg))
-        if 'mode' not in meg:
-            return
-        if meg['mode'] == "2pass-offline":
-            self.on_receive(text)
-
-    def on_error(self, ws, error):
-        util.print(error)
-
-    def on_close(self, ws, close_status_code, close_msg):
-        util.print("### closed ###")
+    def stop(self):
+        self.stop_event.set()
+        if self.thread is not None:
+            self.thread.join()
+            self.thread = None
 
     def send(self, message):
-        with self.lock:
-            if self.ws:
-                self.ws.send(message)
+        if self.thread is None or not self.thread.is_alive():
+            util.print("WebSocket is not started")
+            return
+        self.send_queue.put(message)
 
-    def stop(self):
-        self.stopped = True
-        with self.lock:
-            if self.ws:
-                self.ws.close()
-        self.thread.join()
-        self.thread = None
-        self.ws = None
+    def run_loop(self):
+        async def loop():
+            async with websockets.connect(self.url) as ws:
+                self.ws = ws
+                sender_task = asyncio.create_task(self.sender(ws))
+                receiver_task = asyncio.create_task(self.receiver(ws))
+                await asyncio.gather(sender_task, receiver_task)
+
+        asyncio.run(loop())
+
+    async def sender(self, ws):
+        while not self.stop_event.is_set():
+            try:
+                message = await asyncio.wait_for(self.send_queue.get(), timeout=1)
+                await ws.send(message)
+                self.send_queue.task_done()
+            except asyncio.TimeoutError:
+                continue
+
+    async def receiver(self, ws):
+        while not self.stop_event.is_set():
+            try:
+                message = await asyncio.wait_for(ws.recv(), timeout=1)
+                meg = json.loads(message)
+                text = meg["text"]
+                util.print(str(meg))
+                if 'mode' not in meg:
+                    return
+                if meg['mode'] == "2pass-offline":
+                    self.on_receive(text)
+            except asyncio.TimeoutError:
+                continue
+            except websockets.ConnectionClosed:
+                break
+
