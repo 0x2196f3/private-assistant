e47d3e95acd91269a5a959c5d69387b86f599cc4
0x2196f3 <0x2196f3@gmail.com>
2024-07-15 07:27:08 +0800

    .

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ git fetch --all && git reset --hard origin/main && git pull
 sudo docker build -t private-assistant .
 sudo docker tag private-assistant 0x2196f3/private-assistant:latest
 sudo docker rm -f private-assistant
-sudo docker run -d --name=private-assistant --restart=always --privileged=true -v ../private-assistant-config/config:/config 0x2196f3/private-assistant
+cd ..
+sudo docker run -d --name=private-assistant --restart=always --privileged=true -v ./private-assistant-config/config:/config 0x2196f3/private-assistant
